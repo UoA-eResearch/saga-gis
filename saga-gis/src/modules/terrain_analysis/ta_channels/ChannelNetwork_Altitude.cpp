UoA-eResearch/saga-gis

commit 21f8da821717dba222fd6e82d8fa336132cc907e

saga_api (gui & tools also affected), csg_grid: changed value scaling (and offset) usage
git-svn-id: svn://svn.code.sf.net/p/saga-gis/code-0/trunk@2208 5852f345-3237-4a1a-914e-b28fb4781291

saga-gis/src/modules/terrain_analysis/ta_channels/ChannelNetwork_Altitude.cpp
@@ -242,12 +242,12 @@ void CChannelNetwork_Altitude::Set_Surface(int nCells)
 			//---------------------------------------------
 			if( nz > 0 )
 			{
-				m_Mask.Set_Value(x, y, true);
+				m_Mask.Set_Value(x, y, 1.0);
 				m_Dist.Set_Value(x, y, z / nz);
 			}
 			else
 			{
-				m_Mask.Set_Value(x, y, false);
+				m_Mask.Set_Value(x, y, 1.0);
 
 				if( m_pDistance->is_NoData(x, y) )
 				{

commit 18a6fcaf19c86fb836e8b5a90f6ddc4834ab3c8a

Vertical Distance to Channel Network tool: bug fix grid masking (introduced in r2208, grid value scaling)
git-svn-id: svn://svn.code.sf.net/p/saga-gis/code-0/trunk@2341 5852f345-3237-4a1a-914e-b28fb4781291

saga-gis/src/modules/terrain_analysis/ta_channels/ChannelNetwork_Altitude.cpp
@@ -247,7 +247,7 @@ void CChannelNetwork_Altitude::Set_Surface(int nCells)
 			}
 			else
 			{
-				m_Mask.Set_Value(x, y, 1.0);
+				m_Mask.Set_Value(x, y, 0.0);
 
 				if( m_pDistance->is_NoData(x, y) )
 				{